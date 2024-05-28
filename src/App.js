f29a5f2c5eed39bb24d2210deec63390376c1462
Maflacs <effa87@gmail.com>
2024-05-28 11:44:10 +0200

    Add Modal instead of the alert massage

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Header from './components/Header';
-import Timer from './components/Timer';
-import Knowledge from './components/Knowledge';
-import ClickArea from './components/ClickArea';
-import Learnings from './components/Learnings';
-import Relations from './components/Relations';
-import { getInitialState } from './utils/state';
-import LangContext from './context/LangContext';
-import { ThemeContext } from './context/ThemeContext';
+import React, { useState, useEffect, useContext } from "react";
+import Header from "./components/Header";
+import Timer from "./components/Timer";
+import Knowledge from "./components/Knowledge";
+import ClickArea from "./components/ClickArea";
+import Learnings from "./components/Learnings";
+import Relations from "./components/Relations";
+import { getInitialState } from "./utils/state";
+import LangContext from "./context/LangContext";
+import { ThemeContext } from "./context/ThemeContext";
+import Modal from "./components/Modal";
 
 function App() {
   const [state, setState] = useState(() => {
-    const savedState = localStorage.getItem('gameState');
+    const savedState = localStorage.getItem("gameState");
     return savedState ? JSON.parse(savedState) : getInitialState();
   });
 
-  const { state: { lang } } = useContext(LangContext);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const {
+    state: { lang },
+  } = useContext(LangContext);
 
   const {
     state: { darkMode },
   } = useContext(ThemeContext);
 
   const getClassName = () => {
-     return darkMode ? "bgDark" : "bgLight"
+    return darkMode ? "bgDark" : "bgLight";
   };
 
   const getClassNameContainer = () => {
-    return darkMode ? "bgDarkC" : "bgLightC"
- };
+    return darkMode ? "bgDarkC" : "bgLightC";
+  };
 
   useEffect(() => {
     const intervalId = setInterval(administrateTime, 200);
@@ -35,18 +40,21 @@ function App() {
   }, [state]);
 
   useEffect(() => {
-    localStorage.setItem('gameState', JSON.stringify(state));
+    localStorage.setItem("gameState", JSON.stringify(state));
   }, [state]);
 
   const administrateTime = () => {
     let currentTimestamp = new Date().getTime();
-    let elapsedTime = Math.floor((currentTimestamp - state.startTimestamp) / 1000);
+    let elapsedTime = Math.floor(
+      (currentTimestamp - state.startTimestamp) / 1000
+    );
     let rewardSeconds = elapsedTime - state.sec;
     if (rewardSeconds > 0) {
       setState((prevState) => ({
         ...prevState,
-        knowledge: prevState.knowledge + prevState.knowledgePerSec * rewardSeconds,
-        sec: elapsedTime
+        knowledge:
+          prevState.knowledge + prevState.knowledgePerSec * rewardSeconds,
+        sec: elapsedTime,
       }));
     }
   };
@@ -54,7 +62,7 @@ function App() {
   const handleKnowledgeClicked = () => {
     setState((prevState) => ({
       ...prevState,
-      knowledge: prevState.knowledge + prevState.knowledgePerClick
+      knowledge: prevState.knowledge + prevState.knowledgePerClick,
     }));
   };
 
@@ -65,7 +73,7 @@ function App() {
       newLearnings[index] = {
         ...newLearnings[index],
         amount: newLearnings[index].amount + 1,
-        price: Math.floor(newLearnings[index].price * 1.1) // Increase price by 10%
+        price: Math.floor(newLearnings[index].price * 1.1), // Increase price by 10%
       };
       if (newLearnings[index + 1]) {
         newLearnings[index + 1].unlocked = true;
@@ -73,12 +81,13 @@ function App() {
       setState((prevState) => ({
         ...prevState,
         knowledge: prevState.knowledge - clickedLearning.price,
-        knowledgePerClick: prevState.knowledgePerClick + clickedLearning.knowledgePerClickIncrement,
-        learnings: newLearnings
+        knowledgePerClick:
+          prevState.knowledgePerClick +
+          clickedLearning.knowledgePerClickIncrement,
+        learnings: newLearnings,
       }));
     } else {
-        
-      alert(lang === "hu" ? "Nem áll rendelkezésedre elég tudáspont!" : "You don't have enough knowledge points!");
+      setModalOpen(true);
     }
   };
 
@@ -89,7 +98,7 @@ function App() {
       newRelations[index] = {
         ...newRelations[index],
         amount: newRelations[index].amount + 1,
-        price: Math.floor(newRelations[index].price * 1.1) // Increase price by 10%
+        price: Math.floor(newRelations[index].price * 1.1), // Increase price by 10%
       };
       if (newRelations[index + 1]) {
         newRelations[index + 1].unlocked = true;
@@ -97,38 +106,63 @@ function App() {
       setState((prevState) => ({
         ...prevState,
         knowledge: prevState.knowledge - clickedRelation.price,
-        knowledgePerSec: prevState.knowledgePerSec + clickedRelation.knowledgePerSecIncrement,
-        relations: newRelations
+        knowledgePerSec:
+          prevState.knowledgePerSec + clickedRelation.knowledgePerSecIncrement,
+        relations: newRelations,
       }));
     } else {
-        alert(lang === "hu" ? "Nem áll rendelkezésedre elég tudáspont!" : "You don't have enough knowledge points!");
+      setModalOpen(true);
     }
   };
 
-  const handleRestart = () => {
-    const initialState = getInitialState();
-    localStorage.setItem("gameState", JSON.stringify(initialState));
-    setState(initialState);
-  };
+    const closeModal = () => {
+      setModalOpen(false);
+    };
 
-  return (
-    <div className={getClassName()}>
-      <Header color={getClassNameContainer()} onRestart={handleRestart} />
-      <main className="main-container">
-        <section className="inventory-container perclick">
-          <Learnings color={getClassNameContainer()} learnings={state.learnings} onClick={handleLearningsClicked} />
-        </section>
-        <section className="clicking-area-container xp">
-          <Timer sec={state.sec} />
-          <ClickArea onClick={handleKnowledgeClicked} />
-          <Knowledge state={state} />
-        </section>
-        <section className="inventory-container persec">
-          <Relations color={getClassNameContainer()} relations={state.relations} onClick={handleRelationsClicked} />
-        </section>
-      </main>
-    </div>
-  );
-}
+    const handleRestart = () => {
+      const initialState = getInitialState();
+      localStorage.setItem("gameState", JSON.stringify(initialState));
+      setState(initialState);
+    };
+
+    return (
+      <div className={getClassName()}>
+        <Header color={getClassNameContainer()} onRestart={handleRestart} />
+        <div>
+          {modalOpen && (
+            <Modal color={getClassNameContainer()}
+              message={
+                lang === "hu"
+                  ? "Nem áll rendelkezésedre elég tudáspont!"
+                  : "You don't have enough knowledge points!"
+              }
+              onClose={closeModal}
+            />
+          )}
+        </div>
+        <main className="main-container">
+          <section className="inventory-container perclick">
+            <Learnings
+              color={getClassNameContainer()}
+              learnings={state.learnings}
+              onClick={handleLearningsClicked}
+            />
+          </section>
+          <section className="clicking-area-container xp">
+            <Timer sec={state.sec} />
+            <ClickArea onClick={handleKnowledgeClicked} />
+            <Knowledge state={state} />
+          </section>
+          <section className="inventory-container persec">
+            <Relations
+              color={getClassNameContainer()}
+              relations={state.relations}
+              onClick={handleRelationsClicked}
+            />
+          </section>
+        </main>
+      </div>
+    );
+};
 
 export default App;
